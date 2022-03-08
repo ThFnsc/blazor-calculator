a08644a47bd80431b31f3f77f9abea4cdea35c65
unknown <isaachermel@gmail.com>
2022-03-08 02:28:40 -0300

    Uploading app

---
+++ wwwroot/javascript/script.js
@@ -0,0 +1,7 @@
+function evalCalc(id, expression) {
+  document.getElementById(id).value = eval(expression);
+}
+
+function voceNaoViuNada() {
+  alert("Nada por aqui, apenas ignore este botão... 🤫");
+}
